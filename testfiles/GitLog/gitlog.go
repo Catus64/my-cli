33ad75b11596dd478ddb5849e23cb4cff21af968
rahman <rahman.jazlan@gmail.com>
2025-11-11 17:23:28 +0800

    added some more stuff

--- testfiles/GitLog/gitlog.go
+++ testfiles/GitLog/gitlog.go
@@ -1,9 +1,35 @@
 package gitlog
 
 import (
+	"fmt"
+	read "gocmd/testfiles/GitHashRead"
+	gitobj "gocmd/testfiles/GitObject"
 	gitpath "gocmd/testfiles/Gitrepostruct"
 )
 
-func log(repo gitpath.GitRepository, sha []byte, seen []byte) {
+func Format_date_time(time string) string {
+	return " "
+}
 
+func Log_One(kvlm gitobj.KvlmDict) {
+
+}
+
+func Recurse_Git(commit gitobj.GitCommit) {
+	//log one obj
+
+	//parent = current pbj = end func
+
+	//recurse
+}
+
+func Log(repo gitpath.GitRepository, path string) error {
+	Commit_Object := read.Object_Read(repo, path)
+	Commit_Object.Deserialize()
+	Concrete_Commit, ok := Commit_Object.(*gitobj.GitCommit)
+	if !ok {
+		panic(fmt.Sprintf("%v is not a commit object", path))
+	}
+	fmt.Println(Concrete_Commit.KvlmDict.Dict["parent"])
+	return nil
 }
