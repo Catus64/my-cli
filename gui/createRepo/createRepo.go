8feba5f39931b0736b848bf30518e7594385cfa9
Sim Yi Jie <simyijie2004@gmail.com>
2026-04-15 13:13:11 +0800

    testing create repo

--- gui/createRepo/createRepo.go
+++ gui/createRepo/createRepo.go
@@ -1,11 +1,13 @@
 package createRepo
 
 import (
+	"fmt"
 	"gocmd/gui"
 	"gocmd/gui/help"
 	"gocmd/gui/history"
 	"gocmd/gui/homepage"
 	"gocmd/gui/modifiedFile"
+	gitpath "gocmd/testfiles/Gitrepostruct"
 	"image/color"
 	"strings"
 
@@ -16,17 +18,21 @@ import (
 	"fyne.io/fyne/v2/widget" // UI components (buttons, text boxes)
 )
 
+func RepoInit(path string) {
+	_ = gitpath.Repo_create(path)
+}
+
 func Show(gui *gui.MyApp) {
 	myWindow := gui.App.NewWindow(gui.Window.Title())
 
-    selectedPath := "" 
-    entry := pathEntry(&selectedPath)
-    browseButton := browseButton(myWindow, entry, &selectedPath)
-    createButton := createButton(entry, myWindow, gui)
-    quitButton := QuitButton(myWindow, gui)
-    content := Content(entry, browseButton, createButton, quitButton)
+	selectedPath := ""
+	entry := pathEntry(&selectedPath)
+	browseButton := browseButton(myWindow, entry, &selectedPath)
+	createButton := createButton(entry, myWindow, gui)
+	quitButton := QuitButton(myWindow, gui)
+	content := Content(entry, browseButton, createButton, quitButton)
 
-    fullcontent := container.NewBorder(gui.Header(), nil, gui.SideRect(), nil, content)
+	fullcontent := container.NewBorder(gui.Header(), nil, gui.SideRect(), nil, content)
 
 	myWindow.SetContent(fullcontent)
 	myWindow.Resize(fyne.NewSize(1000, 600))
@@ -34,114 +40,117 @@ func Show(gui *gui.MyApp) {
 }
 
 func pathEntry(selectedPath *string) *widget.Entry {
-    entry := widget.NewEntry()
-    entry.SetText(*selectedPath) 
+	entry := widget.NewEntry()
+	entry.SetText(*selectedPath)
 
-    return entry
+	return entry
 }
 
 func browseButton(window fyne.Window, entry *widget.Entry, selectedPath *string) *fyne.Container {
-    browse := widget.NewButton("Browse", func() {
-        fileDialog := dialog.NewFolderOpen(
-            func(reader fyne.ListableURI, err error) {
-                if reader != nil {
-                    // Update input box
-                    *selectedPath = reader.Path()
-                    entry.SetText(*selectedPath) 
-                }
-            },
-            window,
-        )
-        fileDialog.Show()
-    })
-    return container.NewHBox(browse)
+	browse := widget.NewButton("Browse", func() {
+		fileDialog := dialog.NewFolderOpen(
+			func(reader fyne.ListableURI, err error) {
+				if reader != nil {
+					// Update input box
+					*selectedPath = reader.Path()
+					entry.SetText(*selectedPath)
+				}
+			},
+			window,
+		)
+		fileDialog.Show()
+	})
+	return container.NewHBox(browse)
 }
 
 func createButton(entry *widget.Entry, window fyne.Window, gui *gui.MyApp) *widget.Button {
-    createButton := widget.NewButton("Create", func() {                
-        path := strings.TrimSpace(entry.Text)
+	createButton := widget.NewButton("Create", func() {
+		path := strings.TrimSpace(entry.Text)
 
-        // Window after creating
-        mainWindow := gui.App.NewWindow(gui.Window.Title())
-        mainWindow.Resize(fyne.NewSize(1000, 600))
-       
-        // define all navigation function
-        var showHome, showModified, showHistory, showHelp func()
+		fmt.Println(path)
+		RepoInit(path)
 
-        showHome = func() {
-            homepage.Show(gui, path, mainWindow, showModified, showHistory, showHelp)
-        }
-        showModified = func() {
-            modifiedFile.Show(gui, path, mainWindow, showHome, showHistory, showHelp)
-        }
-        showHistory = func() {
-            history.Show(gui, path, mainWindow, showHome, showModified, showHelp)
-        }
-        showHelp = func() {
-            help.Show(gui, path, mainWindow, showHome, showModified, showHistory)
-        }
+		// Window after creating
+		mainWindow := gui.App.NewWindow(gui.Window.Title())
+		mainWindow.Resize(fyne.NewSize(1000, 600))
 
-        window.Hide()
-        mainWindow.Show()
-        showHome() // show homepage after create
-    })
+		// define all navigation function
+		var showHome, showModified, showHistory, showHelp func()
 
-    createButton.Disable()
+		showHome = func() {
+			homepage.Show(gui, path, mainWindow, showModified, showHistory, showHelp)
+		}
+		showModified = func() {
+			modifiedFile.Show(gui, path, mainWindow, showHome, showHistory, showHelp)
+		}
+		showHistory = func() {
+			history.Show(gui, path, mainWindow, showHome, showModified, showHelp)
+		}
+		showHelp = func() {
+			help.Show(gui, path, mainWindow, showHome, showModified, showHistory)
+		}
 
-    entry.OnChanged = func(text string) {
-        cleanText := strings.TrimSpace(text)
-        
-        if cleanText == "" {
-            createButton.Disable()
-        }else {
-            createButton.Enable()
-        }
-    }
-    createButton.Importance = widget.HighImportance // blue
-    return createButton
+		window.Hide()
+		mainWindow.Show()
+		showHome() // show homepage after create
+	})
+
+	createButton.Disable()
+
+	entry.OnChanged = func(text string) {
+		cleanText := strings.TrimSpace(text)
+
+		if cleanText == "" {
+			createButton.Disable()
+		} else {
+			createButton.Enable()
+		}
+	}
+	createButton.Importance = widget.HighImportance // blue
+	return createButton
 }
 
 func QuitButton(window fyne.Window, gui *gui.MyApp) *widget.Button {
-    quitButton := widget.NewButton("Quit", func() {
-        window.Close()
-        gui.Window.Show()
-    })
-    quitButton.Importance = widget.DangerImportance // red
-    return quitButton
+	quitButton := widget.NewButton("Quit", func() {
+		window.Close()
+		gui.Window.Show()
+	})
+	quitButton.Importance = widget.DangerImportance // red
+	return quitButton
 }
 
 func Content(entry *widget.Entry, browseButton *fyne.Container, createButton *widget.Button, quitButton *widget.Button) fyne.CanvasObject {
-    title := canvas.NewText("Create Repository", color.White)
-    title.TextSize = 56
-    title.TextStyle = fyne.TextStyle{Bold: true}
+	title := canvas.NewText("Create Repository", color.White)
+	title.TextSize = 56
+	title.TextStyle = fyne.TextStyle{Bold: true}
 
-    subtitle := canvas.NewText("Select your file", color.Gray{Y: 150})
-    subtitle.TextSize = 16
+	subtitle := canvas.NewText("Select your file", color.Gray{Y: 150})
+	subtitle.TextSize = 16
 
-    Label := canvas.NewText("Directory:", color.White)
-    Label.TextSize = 28
-    Label.TextStyle = fyne.TextStyle{Bold: true}
+	Label := canvas.NewText("Directory:", color.White)
+	Label.TextSize = 28
+	Label.TextStyle = fyne.TextStyle{Bold: true}
 
-    bottomButton := container.NewBorder(nil, nil, createButton, quitButton, nil)
+	bottomButton := container.NewBorder(nil, nil, createButton, quitButton, nil)
 
-    widthMargin := canvas.NewRectangle(color.Transparent)
-    widthMargin.SetMinSize(fyne.NewSize(50, 0))
+	widthMargin := canvas.NewRectangle(color.Transparent)
+	widthMargin.SetMinSize(fyne.NewSize(50, 0))
 
-    heightMargin := canvas.NewRectangle(color.Transparent)
-    heightMargin.SetMinSize(fyne.NewSize(0, 40))
+	heightMargin := canvas.NewRectangle(color.Transparent)
+	heightMargin.SetMinSize(fyne.NewSize(0, 40))
 
 	content := container.NewVBox(
-        heightMargin,
-        title,
-        subtitle,
-        heightMargin,
-        heightMargin,
-        Label,
-        entry,
-        browseButton,
-        heightMargin,
-        bottomButton,
-    )
+		heightMargin,
+		title,
+		subtitle,
+		heightMargin,
+		heightMargin,
+		Label,
+		entry,
+		browseButton,
+		heightMargin,
+		bottomButton,
+	)
 
-    return container.NewBorder(nil, nil, widthMargin, widthMargin, container.NewPadded(content))
+	return container.NewBorder(nil, nil, widthMargin, widthMargin, container.NewPadded(content))
 }
