6be689065c8f52623d7ce0e3c4e3c3bf897280ce
Sim Yi Jie <simyijie2004@gmail.com>
2026-06-23 03:31:38 +0800

    Modified Help Page in GUI

--- gui/help/content.go
+++ gui/help/content.go
@@ -85,40 +85,67 @@ func HelpContent() fyne.CanvasObject {
 	subtitle := canvas.NewText("Description of GUI action (Scrollable)", color.Gray{Y: 150})
 	subtitle.TextSize = 15
 
-	// 1. Home Page
-	section1Title := sectionTitle("1. Home Page")
-	homeBullet1 := bullet("Remove Button - When user click this button, the file in save list will be removed to file list in Modified File page.")
-	homeBullet2 := bullet("Save Button - When user click this button after fill in messages, the file in save list will save with the message.")
+	// 1. Repository Page
+	section1Title := sectionTitle("1. Repository Page")
+	repoBullet1 := bullet("The file(s) showed in the file list box are the file(s) that saved by user for current version in the repository.")
+	repoBullet2 := bullet("Current Version - When user click this button, user can view the detail of the current version in the repository.")
+	repoBullet3 := bullet("User can click the file in the file list box to view the content of the file in the text area.")
 
-	section1 := container.NewVBox(section1Title, homeBullet1, homeBullet2)
+	section1 := container.NewVBox(section1Title, repoBullet1, repoBullet2, repoBullet3)
 
-	// 2. Modified File
-	section2Title := sectionTitle("2. Modified File Page")
-	modifiedBullet1 := bullet("The file(s) showed in the file list box are the file(s) that modified by user but haven't save to repository.")
-	modifiedBullet2 := bullet("Add Button - When user click this button, the file in file list will add to save list in Home Page.")
+	// 2. Save List Page
+	section2Title := sectionTitle("2. Save List Page")
+	saveListBullet1 := bullet("The file(s) showed in the save list box are the file(s) that user want to save to repository.")
+	saveListBullet2 := bullet("The file(s) showed in the \"File to Save\" box are the file(s) that ready to be saved to repository.")
+	saveListBullet3 := bullet("Remove Button - When user click this button, the file in save list will be removed to file list in File Directory Page.")
+	saveListBullet4 := bullet("Save Button - When user click this button after fill in messages, the file in save list will save with the message.")
 
-	section2 := container.NewVBox(section2Title, modifiedBullet1, modifiedBullet2)
+	section2 := container.NewVBox(section2Title, saveListBullet1, saveListBullet2, saveListBullet3, saveListBullet4)
 
-	// 3. History
-	section3Title := sectionTitle("3. History Page")
-	historyBullet1 := bullet("The page will display history and past changes by user to enable them to browse and compare changes across files.")
-	historyBullet2 := bullet("If user click any version button in the page, the interface will direct user to a new tab with column view format to search for history.")
+	// 3. File Directory Page
+	section3Title := sectionTitle("3. File Directory Page")
+	fileDirectoryBullet1 := bullet("The file(s) showed in the file list box are the file(s) that added or modified by user but haven't save to repository.")
+	fileDirectoryBullet2 := bullet("Add Button - When user click this button, the file(s) in file list will add to save list in Save List Page.")
+
+	section3 := container.NewVBox(section3Title, fileDirectoryBullet1, fileDirectoryBullet2)
+
+	// 4. Ignored File Page
+	section4Title := sectionTitle("4. Ignored File Page")
+	ignoredFileBullet1 := bullet("The file(s) showed in the ignored file list box are the file(s) that user want to ignore.")
+	ignoredFileBullet2 := bullet("View File Type Button - When user click this button, user can view the file type that will be ignored in the ignored file list box and edit it.")
+
+	section4 := container.NewVBox(section4Title, ignoredFileBullet1, ignoredFileBullet2)
+
+	// 5. Save File Page
+	section5Title := sectionTitle("5. Save File Page")
+	saveFileBullet1 := bullet("The file(s) showed in the save file list box are the Save States of the project.")
+	saveFileBullet2 := bullet("Add Button - When user click this button, user are able to add the save state in the project.")
+	saveFileBullet3 := bullet("Switch Button - Before user click this button, user need to select the save state in the row box to switch.")
+	saveFileBullet4 := bullet("Merge Button - Before user click this button, user need to select the save state in the row box to merge.")
+
+	section5 := container.NewVBox(section5Title, saveFileBullet1, saveFileBullet2, saveFileBullet3, saveFileBullet4)
+
+	// 6. Save History Page
+	section6Title := sectionTitle("6. Save History Page")
+	saveHistoryBullet1 := bullet("The page will display history and past changes by user to enable them to browse and compare changes across files.")
+	saveHistoryBullet2 := bullet("User can search the history bubbles in the search bar with history bubbles's hash key to find the specific history bubble.")
+	saveHistoryBullet3 := bullet("User can click the history bubble to view the detail.")
 
 	green  := color.RGBA{R: 100, G: 220, B: 100, A: 255}
 	orange := color.RGBA{R: 255, G: 160, B: 60,  A: 255}
 	white  := color.White
 
-	historyBullet3 := coloredBullet([]struct{ text string; color color.Color }{
+	saveHistoryBullet4 := coloredBullet([]struct{ text string; color color.Color }{
 		{" a) ", white}, {"Merge→xxxxx", green}, {" = Merge branch to main", white},
 	})
-	historyBullet4 := coloredNoBullet([]struct{ text string; color color.Color }{
+	saveHistoryBullet5 := coloredNoBullet([]struct{ text string; color color.Color }{
 		{"b) ", white}, {"Merge→xxxxx", orange}, {" = Merge branch to branch", white},
 	})
 
-	section3 := container.NewVBox(section3Title, historyBullet1, historyBullet2, historyBullet3, historyBullet4)
+	section6 := container.NewVBox(section6Title, saveHistoryBullet1, saveHistoryBullet2, saveHistoryBullet3, saveHistoryBullet4, saveHistoryBullet5)
 
-	// 4. Quit Button
-	section4Title := sectionTitle("4. Quit button allows user to quit to dashboard page.")
+	// 7. Quit Button
+	section7 := sectionTitle("7. Quit button allows user to quit to dashboard page.")
 
 	widthMargin := canvas.NewRectangle(color.Transparent)
 	widthMargin.SetMinSize(fyne.NewSize(30, 0))
@@ -137,7 +164,13 @@ func HelpContent() fyne.CanvasObject {
 		heightMargin,
 		section3,
 		heightMargin,
-		section4Title,
+		section4,
+		heightMargin, 
+		section5,
+		heightMargin,
+		section6,
+		heightMargin,
+		section7,
 		heightMargin,
 	)
 
